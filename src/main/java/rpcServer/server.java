91e26cf10674113d95d604ad4dab5f3e3bab846a
Apple <apple@AppledeMacBook-Pro.local>
2017-12-02 14:23:54 +0800

    add config

--- src/main/java/rpcServer/server.java
+++ src/main/java/rpcServer/server.java
@@ -1,8 +1,11 @@
 package rpcServer;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class server {
     public static void main(String[] args) {
         try {
+            new ClassPathXmlApplicationContext("classpath:server.xml");
 
         } catch (Exception x) {
             x.printStackTrace();
